45edbee5a13f802ec9d1c6a3316940e1d5edabf8
StanB <stanb791@gmail.com>
2024-11-22 16:10:51 +0300

    Добавьте файлы проекта.

---
+++ ArrayProcessorTests1/ArrayProcessor.cs
@@ -0,0 +1,57 @@
+﻿using System;
+
+public class ArrayProcessor
+{
+    // Метод для сортировки массива
+    public int[] SortArray(int[] array)
+    {
+        if (array == null)
+            throw new ArgumentNullException(nameof(array), "Массив не может быть null");
+
+        for (int i = 0; i < array.Length - 1; i++)
+        {
+            for (int j = 0; j < array.Length - i - 1; j++)
+            {
+                if (array[j] > array[j + 1])
+                {
+                    int temp = array[j];
+                    array[j] = array[j + 1];
+                    array[j + 1] = temp;
+                }
+            }
+        }
+
+        return array;
+    }
+
+    // Метод для нахождения максимального числа
+    public int FindMax(int[] array)
+    {
+        if (array == null || array.Length == 0)
+            throw new ArgumentException("Массив не может быть пустым или null");
+
+        int max = array[0];
+        foreach (int num in array)
+        {
+            if (num > max)
+                max = num;
+        }
+
+        return max;
+    }
+
+    // Метод для вычисления среднего арифметического
+    public double CalculateAverage(int[] array)
+    {
+        if (array == null || array.Length == 0)
+            throw new ArgumentException("Массив не может быть пустым или null");
+
+        int sum = 0;
+        foreach (int num in array)
+        {
+            sum += num;
+        }
+
+        return (double)sum / array.Length;
+    }
+}
---
+++ ArrayProcessorTests1/ArrayProcessorTests.cs
@@ -0,0 +1,86 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+[TestClass]
+public class ArrayProcessorTests
+{
+    private ArrayProcessor processor;
+
+    [TestInitialize]
+    public void Setup()
+    {
+        processor = new ArrayProcessor();
+    }
+
+    // Тесты для SortArray
+    [TestMethod]
+    public void SortArray_ShouldSortArrayCorrectly()
+    {
+        int[] input = { 5, 3, 8, 1 };
+        int[] expected = { 1, 3, 5, 8 };
+        int[] result = processor.SortArray(input);
+        CollectionAssert.AreEqual(expected, result);
+    }
+
+    [TestMethod]
+    [ExpectedException(typeof(ArgumentNullException))]
+    public void SortArray_ShouldThrowException_WhenArrayIsNull()
+    {
+        processor.SortArray(null);
+    }
+
+    [TestMethod]
+    public void SortArray_ShouldHandleEmptyArray()
+    {
+        int[] input = { };
+        int[] result = processor.SortArray(input);
+        CollectionAssert.AreEqual(new int[] { }, result);
+    }
+
+    // Тесты для FindMax
+    [TestMethod]
+    public void FindMax_ShouldReturnCorrectMax()
+    {
+        int[] input = { 1, 7, 3, 9 };
+        int result = processor.FindMax(input);
+        Assert.AreEqual(9, result);
+    }
+
+    [TestMethod]
+    [ExpectedException(typeof(ArgumentException))]
+    public void FindMax_ShouldThrowException_WhenArrayIsEmpty()
+    {
+        processor.FindMax(new int[] { });
+    }
+
+    [TestMethod]
+    public void FindMax_ShouldReturnElement_WhenArrayHasOneElement()
+    {
+        int[] input = { 42 };
+        int result = processor.FindMax(input);
+        Assert.AreEqual(42, result);
+    }
+
+    // Тесты для CalculateAverage
+    [TestMethod]
+    public void CalculateAverage_ShouldReturnCorrectAverage()
+    {
+        int[] input = { 2, 4, 6, 8 };
+        double result = processor.CalculateAverage(input);
+        Assert.AreEqual(5.0, result, 0.001);
+    }
+
+    [TestMethod]
+    [ExpectedException(typeof(ArgumentException))]
+    public void CalculateAverage_ShouldThrowException_WhenArrayIsEmpty()
+    {
+        processor.CalculateAverage(new int[] { });
+    }
+
+    [TestMethod]
+    public void CalculateAverage_ShouldReturnElement_WhenArrayHasOneElement()
+    {
+        int[] input = { 10 };
+        double result = processor.CalculateAverage(input);
+        Assert.AreEqual(10.0, result, 0.001);
+    }
+}
